607a6054a14b8f64ab3c921837b9c39111e38815
Karishma Gupta <40417973+guptakarishma0304@users.noreply.github.com>
2020-12-31 14:26:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 // Configure the Google Cloud provider
 provider "google" {
-  credentials = file("kubernetes-practice-db1b1efd7863.json")
+  //credentials = file("kubernetes-practice-db1b1efd7863.json")
   project     = "kubernetes-practice-db1b1efd7863"
   region      = "us-west1"
   version     = "~> 3.51"
